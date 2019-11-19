98a79bf9d1a525e01ed343209f2eafd35e92bd6f
Chris Eckhardt <ceckhardt06@gmail.com>
2019-11-18 20:18:33 -0800

    component templates added

---
+++ switter/src/components/tweets.jsx
@@ -0,0 +1,10 @@
+class tweets extends React.Component {
+
+    //////////// THIS IS NOT COMPLETE, JUST PLACEHOLDER
+
+    render() { // THIS ENDPOINT DOES NOT EXITS YET
+        <div>
+            <tweets_list /> 
+        </div>
+    }
+}
