56552ab83a30120198591dd606ea3a09838d2cda
jimmychen10 <jimmychen1996@yahoo.com>
2019-11-20 04:39:31 -0800

    WIP

--- switter/src/App.js
+++ switter/src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import twitterLogo from './pictures/twitter.png';
+
+// import {BrowserRouter,  Link} from "react-router-dom";
+// this is the main page, I think probabl;y we should create a seperate page for that
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <img src={twitterLogo} className="logo" />
+      <h1>Home</h1> */}
+      <div className="container">
+        <div className="leftpane">
+        <img src={twitterLogo} className="logo"/>
+        </div>
+        <div className="middlepane">
+          <h1>Home</h1>
+          <input type="text" name="firstname" value="Mickey"></input>
+        
+        </div>
+        <div className="rightpane">
+          <h1>Test Page</h1>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ switter/src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class MainPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Your in the main Page :D</h1>
+            </div>
+        )
+    }
+}
+
+export default MainPage
