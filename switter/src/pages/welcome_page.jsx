dd7fff4f21fe474fa20eace9083cba08ea7cfcf6
Sunny Srijan <sunnysrijan@gmail.com>
2019-11-24 15:21:18 -0800

    Welcome_Page for sigin and singup

--- switter/src/pages/welcome_page.jsx
+++ switter/src/pages/welcome_page.jsx
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
-import {HashRouter as Link, NavLink} from 'react-router-dom';
-//import { Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
 
-const SigninTab = () => {
+
+import './App.css';
+
+class App extends Component {
+  render() {
     return (
-        <div className="App__Form"> 
-          <div className="PageSwitcher">
+      <Router basename="/react-auth-ui/">
+        <div className="App">
+          <div className="App__Aside"></div>
+          <div className="App__Form">
+            <div className="PageSwitcher">
                 <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
                 <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
               </div>
 
-              <div className="text">
-                <h1>See what's happening in </h1>
-                <h1>the world right now</h1>
-              </div>
-                
               <div className="FormTitle">
-                  <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or
+                  <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or 
                   <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
               </div>
-          
+
+              
           </div>
 
+        </div>
+      </Router>
     );
+  }
 }
-export default SigninTab;
+
+export default welcome_page;
